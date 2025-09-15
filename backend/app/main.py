9a4ba5dfd782bdd5c4da5b7cd0c7558f1e344511
muhammadfaizz02 <114665852+muhammadfaizz02@users.noreply.github.com>
2025-09-15 18:44:34 +0700

    Update main.py

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-# from .database import engine, Base  # COMMENT
+from .database import engine, Base  
 from .routers import products, orders
 import os
 
